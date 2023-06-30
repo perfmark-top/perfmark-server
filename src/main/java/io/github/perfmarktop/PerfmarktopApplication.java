e66b7ce9c02d7b8514564e0e911e7992a40f4b99
Luke <17609267054@163.com>
2023-06-30 14:15:22 +0800

    Commit message

---
+++ src/main/java/io/github/perfmarktop/PerfmarktopApplication.java
@@ -0,0 +1,13 @@
+package io.github.perfmarktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfmarktopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfmarktopApplication.class, args);
+    }
+
+}
